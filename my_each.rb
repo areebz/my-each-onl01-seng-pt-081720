f0f0e16a9364e588e3af4d3d05df25d83ebbb17f
Areeba Zahir <areebazahir3@gmail.com>
2020-08-30 06:00:23 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,67 +1,28 @@
+list = ["ab", "cd", "ef"]
+
 def my_each(array)
-  if block_given?
-    i = 0
-    
+  i = 0
   while i < array.length
-  yield(array[i])
-  i = i + 1
+# The "while" method will always return a value of => nil"
+    yield(array[i])
+    i = i + 1
+  end
 end
 
-array
-end
-end 
+my_each(list) {|i| puts "So I think #{i} is a piece of shit"}
 
+# Use the collection method 
+# To make the *return-value* the answer you want
+# instead of "=> nil"
 
-def does_not_call_on(array)
-  if block_given?
-    i = 0
-    
-  while i < array.not_to include(".each")
-  yield(array[i])
-  i = i + 1
-end
-end 
-
-
-
-passengers = {
-   suite_a: "Amanda Presley",
-   suite_b: "Seymour Hoffman",
-   suite_c: "Alfred Tennyson",
-   suite_d: "Charlie Chaplin",
-   suite_e: "Crumpet the Elf"
-}
-
-def select_winner(passengers)
-   winner = ""
-   passengers.each { |suite, name|
-      if suite == :suite_a && name.start_with?("A")
-      winner = name
-      end
-   }
-   winner
+def hello(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
 end
 
-def key_for_min_value(name_hash)
-   return nil if name_hash.size == 0
-   min_key, min_value = name_hash.first
-   name_hash.each do |key, value|
-      if value < min_value
-      min_key = key #just need the key
-      end
-   end
-   min_key
-end
-
-
-def key_for_min_value(name_hash)
-   min_key = nil
-   min_value = nil
-   name_hash.each do |key, value|
-      if min_value == nil || value < min_value
-         min_value = value
-         min_key = key
-      end
-   end
-   min_key
-end
+hello(list) {|i| "Hello #{i} what's up?"}
